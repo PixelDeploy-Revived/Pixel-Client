7b26504eb587a5d3c1ac4045750189022ddde75b
XenKys <sergiotuzzo@gmail.com>
2026-05-08 09:59:46 +0200

    chore: version

--- src/minecraft/pixel/Pixel.java
+++ src/minecraft/pixel/Pixel.java
@@ -25,7 +25,7 @@ public class Pixel {
 	private static final Pixel instance = new Pixel();
 	private static final Minecraft mc = Minecraft.getMinecraft();
 	
-	private static final String COMMIT = "df75e45";
+	private static final String COMMIT = "3f2e826";
 	private static final String BRANCH = "main";
 	
 	private final Discord discord = new Discord();
