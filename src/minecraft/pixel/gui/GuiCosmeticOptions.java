ac6a4e584bfe2064cbff1f5e9525fe29b1fa63c1
XenKys <sergiotuzzo@gmail.com>
2026-05-14 15:35:08 +0200

    fix: paging

--- src/minecraft/pixel/gui/GuiCosmeticOptions.java
+++ src/minecraft/pixel/gui/GuiCosmeticOptions.java
@@ -81,7 +81,7 @@ public class GuiCosmeticOptions extends GuiScreen {
 				buttonList.add(new GuiButton(-2, (width - 200) / 2 - 2 - 20, height / 6 + 168, 20, 20, "<"));
 			}
 			
-			if (page < totPages && page + 1 != totPages) {
+			if (page < totPages) {
 				buttonList.add(new GuiButton(-3, (width + 200) / 2 + 2, height / 6 + 168, 20, 20, ">"));
 			}
 		}
